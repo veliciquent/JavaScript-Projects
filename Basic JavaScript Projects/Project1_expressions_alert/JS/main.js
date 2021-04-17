ab2289183e071e6b0cb6742fb877d727085ff425
Joshua Ramos <veliciquent@gmail.com>
2021-04-17 13:35:42 -0600

    Project1 Expressions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var x = "\"This is a String\""
+x += " so please be curteous"
+
+var win = "CHICKEN", lose = "LOSBSTER", timesUp= "DINNER" // Defines Variables
+var lose = lose.fontcolor("red"), timesUp = timesUp.fontcolor("purple"); // Redefines with color
+win = win.fontcolor("orange");
+document.write(x); //Writes the stuffs
+document.write(win + timesUp);
+window.alert(x);
+document.write(2 * 13)
